e18db65c1a0f4cdd64076b6bfadf4e8265e76c00
Wiktor Kycia <wiktor.kycia@gmail.com>
2024-09-03 17:48:47 +0200

    use the Taskscontext in Task conponent

--- src/components/Task/Task.tsx
+++ src/components/Task/Task.tsx
@@ -1,11 +1,12 @@
-import {TaskType} from "../../context/TaskContext.ts";
-
+import {TasksContext, TaskType} from "../../context/TaskContext.ts";
+import {useContext} from "react";
 interface TaskComponentType{
     task: TaskType
 }
 
 export default function Task({task}:TaskComponentType) {
-
+    const context = useContext(TasksContext);
+    const {tasks, setTasks} = context;
     let mainContent:JSX.Element = <p>{task}</p>;
     return (
         <div>

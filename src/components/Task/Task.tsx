2d07067ecb60aa853adc7a1955504d1400ef3e89
Wiktor Kycia <wiktor.kycia@gmail.com>
2024-09-06 08:23:11 +0200

    make two hook variables, bool useState and html useRef

--- src/components/Task/Task.tsx
+++ src/components/Task/Task.tsx
@@ -1,5 +1,5 @@
 import {TasksContext, TaskType} from "../../context/TaskContext.ts";
-import {useContext, useEffect, useRef, useState} from "react";
+import {useContext,  useRef, useState} from "react";
 interface TaskComponentType{
     task: TaskType
 }
@@ -8,53 +8,29 @@ export default function Task({task}:TaskComponentType) {
     const context = useContext(TasksContext);
     const {tasks, setTasks} = context;
     const [this_task, setThisTask] = useState<TaskType>(task);
-    const [mainContent, setMainContent] = useState<React.ReactNode>(<p>{this_task.text}</p>);
-    const [inputValue, setInputValue] = useState<string>(this_task.text);
 
-    const inputRef = useRef<HTMLInputElement | null>(null);
+    const [isInput, setIsInput] = useState<boolean>(false);
+    const mainContent = useRef(<p>{this_task.text}</p>);
 
     function handleInputSubmit(){
-        setMainContent(<p>{this_task.text}</p>);
+        setMainContent();
     }
 
     const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-        console.log(event.target.value)
-        setInputValue(event.target.value);
-        setThisTask({...this_task, text : inputValue});
+
         handleEdit();
     };
     function handleEdit() {
-        setMainContent(
+        mainContent.current = (
             <input
                 type="text"
                 onChange={handleInputChange}
                 value={inputValue}
-                ref={inputRef}
+                ref={mainContent}
             />
         );
+        setIsInput(true);
     }
-    useEffect(() => {
-        if (inputRef.current) {
-            const handleKeyUp = (event: KeyboardEvent) => {
-                if (event.key === "Enter") {
-                    handleInputSubmit();
-                }
-            };
-
-            if ("addEventListener" in inputRef.current) {
-                inputRef.current.addEventListener("keyup", handleKeyUp);
-            }
-
-            // Cleanup function to remove the event listener when the component unmounts
-            return () => {
-                if (inputRef.current) {
-                    if ("removeEventListener" in inputRef.current) {
-                        inputRef.current.removeEventListener("keyup", handleKeyUp);
-                    }
-                }
-            };
-        }
-    }, [inputRef.current, inputValue]); // Dependencies include the ref and inputValue
 
     return (
         <div>
